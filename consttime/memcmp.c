51c48b86cdc15060512a41a4be5a5ebbb9d2dee0
Marc Schoolderman <squell@xs4all.nl>
2016-03-02 22:43:50 +0100

    constant time memcmp (two's complement, requires arithmetic right-shift)

---
+++ consttime/memcmp.c
@@ -0,0 +1,21 @@
+/* Constant time memmcp */
+
+#include <limits.h>
+#include <stdio.h>
+
+int memcmp_consttime(const void *s1, const void *s2, size_t n)
+{
+    unsigned const char *p1 = s1, *p2 = s2;
+    int c = 0;
+    while(n--)
+        c |= (*p1++ - *p2++) & ~(-c|c) >> CHAR_BIT;
+    return c;
+}
+
+int main()
+{
+    printf("%d\n", memcmp_consttime("abc","abc", 3));
+    printf("%d\n", memcmp_consttime("abc","ada", 3));
+    printf("%d\n", memcmp_consttime("abc","aad", 3));
+    return 0;
+}
